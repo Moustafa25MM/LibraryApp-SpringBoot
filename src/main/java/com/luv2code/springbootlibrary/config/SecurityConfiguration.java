478b910ced3502387a5db575f9a3ce30793acdf7
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-26 09:35:50 +0300

    Security Configuration

---
+++ src/main/java/com/luv2code/springbootlibrary/config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.luv2code.springbootlibrary.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // Disable CSRF
+//         http.csrf().disable();
+
+        // protect endpoints at /api/<type>/secure
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/books/secure/**").authenticated()
+                .anyRequest()
+                .permitAll());
+
+        // Add Cors filters
+        http.cors(Customizer.withDefaults());
+
+        //Add content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class,
+                new HeaderContentNegotiationStrategy());
+
+        // force a non-empty response to 401
+        Okta.configureResourceServer401ResponseBody(http);
+        return http.build();
+    }
+}
